aff8898584819388aa2c5b7a51dce7f0b85df17a
Aaronidicula <70788358+Aaronidicula@users.noreply.github.com>
2024-04-19 20:46:15 +0530

    human_detection_yolov8.py

--- human_detection_yolov8.py
+++ human_detection_yolov8.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""human_detection yolov8.ipynb
+#human_detection yolov8.ipynb
 
 
 #install ultralytics for yolo v8
@@ -8,4 +8,4 @@ pip install ultralytics
 #import pretrained model and tran it with custom dataset
 from ultralytics import YOLO
 model = YOLO("yolov8n.pt")
-model.train(data = "/content/drive/MyDrive/Human_detection_yolov8/Human detection.v2i.yolov8/data.yaml", epochs = 50)
+model.train(data = "Path/data.yaml", epochs = 50)
